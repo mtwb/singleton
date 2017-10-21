5eebb66156faa9c4ef7a9ad304d6d568832e48b5
Marcin Wisniewski <marcinwisniewski4@gmail.com>
2017-10-21 17:02:17 +0200

    Initial commit

---
+++ Singleton/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    //jezeli damy sealed ,zablokujemy dziedziczenie klasy
+    public class Singleton
+    {
+        //prywante pole, ktore zawiera instancje swojej klasy, domyslnie na null
+        private static Singleton instance_variable = null;
+
+        // blokada przed dostepem wielu watkow, tworzy obiekt dopiero jak jest potrzebny
+        private static readonly object pad_lock = new object();
+
+        private int suma = 0;
+        
+        //publiczna statyczna wlasnosc ktora zwraca obiekt swojej klasy
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (pad_lock)
+                {
+                    if (instance_variable == null)
+                    {
+                        instance_variable = new Singleton();
+                    }
+                    return instance_variable;
+                }
+            }
+        }
+
+        public void  Dodaj_liczby(int a, int b)
+        {           
+            suma = a + b;
+            Console.WriteLine("Suma dwoch liczb "+ suma); 
+        }
+
+        //prywatny konstruktor, blokuje mozliwosc tworzenia obiektow normalna droga,tylko za pomoca statycznej wlasciwosci
+        private Singleton()
+        {
+        }
+    }
+
+    public class Program
+    {/*
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Iteracja {0}", i + 1);
+                Singleton.Instance.Dodaj_liczby(i, i+2);
+            }
+            Console.ReadKey();
+        }*/
+    }
+
+}
+
---
+++ Singleton/Program2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    // typy generyczne
+    public class Singleton2<T> where T : class, new()
+    {
+        //prywante pole, ktore zawiera instancje swojej klasy, domyslnie na null
+        private static T instance_variable = null;
+
+        // blokada przed dostepem wielu watkow, tworzy obiekt dopiero jak jest potrzebny
+        private static readonly object pad_lock = new object();
+
+        protected int suma = 0;
+        //publiczna statyczna wlasnosc ktora zwraca obiekt swojej klasy
+        public static T GetInstance()
+        {
+            lock (pad_lock)
+            {
+                if (instance_variable == null)
+                {
+                    instance_variable = new T();
+                }
+                return instance_variable;
+            }
+        }
+
+        public void Dodaj_liczby()
+        {
+            this.suma = this.suma + 5;
+            Console.WriteLine("Suma dwoch liczb " + this.suma);
+        }
+
+    }
+
+    public class Dzialania : Singleton2<Dzialania>
+    {
+        public void Dodaj()
+        {
+            Console.WriteLine("Dziedziczenie " + suma);
+        }
+    }
+
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Singleton2<Dzialania> obiekt = Dzialania.GetInstance();
+            obiekt.Dodaj_liczby();
+            obiekt = Dzialania.GetInstance();
+            obiekt.Dodaj_liczby();
+
+            Dzialania dzialania = new Dzialania();
+            dzialania.Dodaj();
+
+            obiekt = Dzialania.GetInstance();
+            obiekt.Dodaj_liczby();
+
+            Console.ReadKey();
+        }
+    }
+}
